6d2023b4c12d06b8e63444b0fdeab61a648b08c3
marcos.xavier <sis.marcosxavier@gmail.com>
2022-06-23 11:18:32 -0300

    configurando application.yml para acesso ao banco de dados e alterando a entity Anime

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -3,11 +3,21 @@ package academy.devdojo.springboot2.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Entity
 public class Anime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty("id")
     private Long id;
     @JsonProperty("name")
