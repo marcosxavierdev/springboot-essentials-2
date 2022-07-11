07057deb258688ccd15f3b7888ee2cf5e5c68a39
marcos.xavier <sis.marcosxavier@gmail.com>
2022-07-11 11:05:58 -0300

    criando um response de exception e um handler

---
+++ src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
@@ -0,0 +1,27 @@
+package academy.devdojo.springboot2.handler;
+
+import academy.devdojo.springboot2.exception.BadRequestException;
+import academy.devdojo.springboot2.exception.BadRequestExceptionDetail;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetail> handlerBadRequestException(BadRequestException badRequestException){
+        return new ResponseEntity<>(
+                BadRequestExceptionDetail
+                        .builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(badRequestException.getMessage())
+                        .developerMessage(badRequestException.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+}
